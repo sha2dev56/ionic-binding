33335b583410725ae185350b3323958d02cd0b57
sha2dev56 <sha2dev56@gmail.com>
2026-09-23 17:09:43 +0200

    feat: template ionic

--- src/app/galeria/galeria.component.ts
+++ src/app/galeria/galeria.component.ts
@@ -1,18 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from '../usuario';
+import { Servicio } from '../servicio';
+import { IonContent, IonHeader, IonItem, IonLabel, IonList, IonTitle, IonToolbar} from '@ionic/angular';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-galeria',
   templateUrl: './galeria.component.html',
   styleUrls: ['./galeria.component.scss'],
-  imports: [],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel],
 })
 export class GaleriaComponent  implements OnInit {
   public tituloSeccion: string = 'Galería de imágenes Dinámica';
   public descripcion: string = 'Esta imagen se carga desde el directorio de assets mediante Property Binding';
   public rutaImagenLocal: string = 'assets/foto1.jpg';
 
-  constructor() { }
+  users:Usuario[] = [];
+  constructor(private usersService:Servicio) { }
 
-  ngOnInit() {}
+  async ngOnInit() {
+   this.users= await this.usersService.getUsuarios(); //await se usa para que te vaya dando los usuarios cuando los tenga, si no se usa, la pagina se puede bloquear hasta que cargue el metodo
+  }
 
 }
--- src/app/servicio.ts
+++ src/app/servicio.ts
@@ -1,10 +1,11 @@
-import { Service } from '@angular/core';
+import { Injectable, Service } from '@angular/core';
 import { Usuario } from './usuario';
 
-@Service()
+@Injectable({providedIn: 'root'})
+
 export class Servicio {
 
-    private users: Usuario[]= [
+    private users: Usuario[]= [ //array con items <k,v>, (array de diccionarios)
         {id:1, name: "Luis", email: "luis@gmail.com", active: true},
         {id:2, name: "Maria", email: "maria@gmail.com", active: false},
         {id:3, name: "Pablo", email: "pablo@gmail.com", active: true},
